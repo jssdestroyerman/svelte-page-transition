9c323b3773c088c34b864f4c1928a1baeacf67fa
jssdestroyerman <jesuisdestroyerman@proton.me>
2023-03-09 13:51:45 +0100

    first attempt

---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { gsap } from 'gsap';
+
+	function spin(node: any) {
+		gsap.to(node, {
+			rotate: 1080
+		});
+		return {};
+	}
+	function spinOut(node: any) {
+		gsap.to(node, {
+			rotate: -1080
+		});
+		return {};
+	}
+</script>
+
+<div class="centered" in:spin out:spinOut>
+	<span>transitions!</span>
+</div>
+<a href="/about">about page</a>
+
+<style>
+	.centered {
+		position: absolute;
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%, -50%);
+	}
+
+	span {
+		position: absolute;
+		transform: translate(-50%, -50%);
+		font-size: 4em;
+	}
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,3 @@
+<h1 class="h1">about page</h1>
+
+<a href="/">home page</a>
